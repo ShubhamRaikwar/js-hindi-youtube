3b524ff2031f2a2b84319adf71d6aa751c2383f2
Shubham Raikwar <raikwar_shubham@yahoo.co.in>
2024-05-09 05:25:52 +0000

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 471201;
+let accountEmail = "shubham@google.com";
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2; 
+
+accountEmail = "sr@sr.com"
+accountPassword = "56478"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(2 + 5);  // we are using Node.js not browser   
+
+let name = "Shubham";
+let age = 18
+let isLoggedIn = true
+let state
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
