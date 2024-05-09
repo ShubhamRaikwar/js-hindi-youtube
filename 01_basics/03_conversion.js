da2ede1efb78bfc7fb369cb693735601b4bfcf4e
Shubham Raikwar <raikwar_shubham@yahoo.co.in>
2024-05-09 06:23:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+let score = "shubham"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "91" => 91
+// "91abc" => NaN
+// true => 1; fasle => 0
+
+let isLoggedIn = "shubham"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "shubham" => true
+
+let someNumber = 99
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
